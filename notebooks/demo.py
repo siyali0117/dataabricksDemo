7c94b0e40922d45197c8415800e648dd0ee538ff
siyali0117 <89073425+siyali0117@users.noreply.github.com>
2021-09-21 23:04:49 +0800

    new test

--- notebooks/demo.py
+++ notebooks/demo.py
@@ -1,1 +1,389 @@
+# Databricks notebook source
+# DBTITLE 1,Training machine learning models on tabular data: an end-to-end example
+This tutorial covers the following steps:
+
+Import data from your local machine into the Databricks File System (DBFS)
+Visualize the data using Seaborn and matplotlib
+Run a parallel hyperparameter sweep to train machine learning models on the dataset
+Explore the results of the hyperparameter sweep with MLflow
+Register the best performing model in MLflow
+Apply the registered model to another dataset using a Spark UDF
+Set up model serving for low-latency requests
+In this example, you build a model to predict the quality of Portugese "Vinho Verde" wine based on the wine's physicochemical properties.
+
+The example uses a dataset from the UCI Machine Learning Repository, presented in Modeling wine preferences by data mining from physicochemical properties [Cortez et al., 2009].
+
+# COMMAND ----------
+
+# MAGIC %pip install --upgrade cloudpickle
+
+# COMMAND ----------
+
+# DBTITLE 1,Import data
+# If you have the File > Upload Data menu option, follow the instructions in the previous cell to upload the data from your local machine.
+# The generated code, including the required edits described in the previous cell, is shown here for reference.
+ 
+import pandas as pd
+ 
+# In the following lines, replace <username> with your username.
+# white_wine = pd.read_csv("/dbfs/FileStore/shared_uploads/<username>/winequality_white.csv", sep=';')
+# red_wine = pd.read_csv("/dbfs/FileStore/shared_uploads/<username>/winequality_red.csv", sep=';')
+ 
+# If you do not have the File > Upload Data menu option, uncomment and run these lines to load the dataset.
+ 
+white_wine = pd.read_csv("/dbfs/databricks-datasets/wine-quality/winequality-white.csv", sep=";")
+red_wine = pd.read_csv("/dbfs/databricks-datasets/wine-quality/winequality-red.csv", sep=";")
+
+# COMMAND ----------
+
+# Merge the two DataFrames into a single dataset, with a new binary feature "is_red" that indicates whether the wine is red or white.
+
+red_wine['is_red'] = 1
+white_wine['is_red'] = 0
+ 
+data = pd.concat([red_wine, white_wine], axis=0)
+ 
+# Remove spaces from column names
+data.rename(columns=lambda x: x.replace(' ', '_'), inplace=True)
+
+# COMMAND ----------
+
+data.head()
+
+# COMMAND ----------
+
+# DBTITLE 1,Visualize data
+# Plot a histogram of the dependent variable, quality.
+
+import seaborn as sns
+sns.distplot(data.quality, kde=False)
+
+# COMMAND ----------
+
+# Define a wine as high quality if it has quality >= 7.
+
+high_quality = (data.quality >= 7).astype(int)
+data.quality = high_quality
+
+# COMMAND ----------
+
+# Box plots are useful in noticing correlations between features and a binary label.
+import matplotlib.pyplot as plt
+ 
+dims = (3, 4)
+ 
+f, axes = plt.subplots(dims[0], dims[1], figsize=(25, 15))
+axis_i, axis_j = 0, 0
+for col in data.columns:
+  if col == 'is_red' or col == 'quality':
+    continue # Box plots cannot be used on indicator variables
+  sns.boxplot(x=high_quality, y=data[col], ax=axes[axis_i, axis_j])
+  axis_j += 1
+  if axis_j == dims[1]:
+    axis_i += 1
+    axis_j = 0
+
+# COMMAND ----------
+
+# DBTITLE 1,Preprocess data
+# Prior to training a model, check for missing values and split the data into training and validation sets.
+
+data.isna().any()
+
+# COMMAND ----------
+
+# There are no missing values.
+
+from sklearn.model_selection import train_test_split
+ 
+train, test = train_test_split(data, random_state=123)
+X_train = train.drop(["quality"], axis=1)
+X_test = test.drop(["quality"], axis=1)
+y_train = train.quality
+y_test = test.quality
+
+# COMMAND ----------
+
+# DBTITLE 1,Build a baseline model
+# This task seems well suited to a random forest classifier, since the output is binary and there may be interactions between multiple variables.
+
+# The following code builds a simple classifier using scikit-learn. It uses MLflow to keep track of the model accuracy, and to save the model for later use.
+
+import mlflow
+import mlflow.pyfunc
+import mlflow.sklearn
+import numpy as np
+import sklearn
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import roc_auc_score
+from mlflow.models.signature import infer_signature
+from mlflow.utils.environment import _mlflow_conda_env
+import cloudpickle
+import time
+ 
+# The predict method of sklearn's RandomForestClassifier returns a binary classification (0 or 1). 
+# The following code creates a wrapper function, SklearnModelWrapper, that uses 
+# the predict_proba method to return the probability that the observation belongs to each class. 
+ 
+class SklearnModelWrapper(mlflow.pyfunc.PythonModel):
+  def __init__(self, model):
+    self.model = model
+    
+  def predict(self, context, model_input):
+    return self.model.predict_proba(model_input)[:,1]
+ 
+# mlflow.start_run creates a new MLflow run to track the performance of this model. 
+# Within the context, you call mlflow.log_param to keep track of the parameters used, and
+# mlflow.log_metric to record metrics like accuracy.
+with mlflow.start_run(run_name='untuned_random_forest'):
+  n_estimators = 10
+  model = RandomForestClassifier(n_estimators=n_estimators, random_state=np.random.RandomState(123))
+  model.fit(X_train, y_train)
+ 
+  # predict_proba returns [prob_negative, prob_positive], so slice the output with [:, 1]
+  predictions_test = model.predict_proba(X_test)[:,1]
+  auc_score = roc_auc_score(y_test, predictions_test)
+  mlflow.log_param('n_estimators', n_estimators)
+  # Use the area under the ROC curve as a metric.
+  mlflow.log_metric('auc', auc_score)
+  wrappedModel = SklearnModelWrapper(model)
+  # Log the model with a signature that defines the schema of the model's inputs and outputs. 
+  # When the model is deployed, this signature will be used to validate inputs.
+  signature = infer_signature(X_train, wrappedModel.predict(None, X_train))
+  
+  # MLflow contains utilities to create a conda environment used to serve models.
+  # The necessary dependencies are added to a conda.yaml file which is logged along with the model.
+  conda_env =  _mlflow_conda_env(
+        additional_conda_deps=None,
+        additional_pip_deps=["cloudpickle=={}".format(cloudpickle.__version__), "scikit-learn=={}".format(sklearn.__version__)],
+        additional_conda_channels=None,
+    )
+  mlflow.pyfunc.log_model("random_forest_model", python_model=wrappedModel, conda_env=conda_env, signature=signature)
+
+
+# COMMAND ----------
+
+# Examine the learned feature importances output by the model as a sanity-check.
+
+feature_importances = pd.DataFrame(model.feature_importances_, index=X_train.columns.tolist(), columns=['importance'])
+feature_importances.sort_values('importance', ascending=False)
+
+# COMMAND ----------
+
+# DBTITLE 1,Register the model in MLflow Model Registry
+# By registering this model in Model Registry, you can easily reference the model from anywhere within Databricks.
+
+run_id = mlflow.search_runs(filter_string='tags.mlflow.runName = "untuned_random_forest"').iloc[0].run_id
+# If you see the error "PERMISSION_DENIED: User does not have any permission level assigned to the registered model", 
+# the cause may be that a model already exists with the name "wine_quality". Try using a different name.
+model_name = "wine_quality"
+model_version = mlflow.register_model(f"runs:/{run_id}/random_forest_model", model_name)
+ 
+# Registering the model takes a few seconds, so add a small delay
+time.sleep(15)
+
+# COMMAND ----------
+
+# You should now see the model in the Models page. To display the Models page, click the Models icon in the left sidebar.
+
+# Next, transition this model to production and load it into this notebook from Model Registry.
+
+from mlflow.tracking import MlflowClient
+ 
+client = MlflowClient()
+client.transition_model_version_stage(
+  name=model_name,
+  version=model_version.version,
+  stage="Production",
+)
+
+# COMMAND ----------
+
+# The Models page now shows the model version in stage "Production".
+
+model = mlflow.pyfunc.load_model(f"models:/{model_name}/production")
+ 
+# Sanity-check: This should match the AUC logged by MLflow
+print(f'AUC: {roc_auc_score(y_test, model.predict(X_test))}')
+
+# COMMAND ----------
+
+# DBTITLE 1,Experiment with a new model
+# The random forest model performed well even without hyperparameter tuning.
+
+# The following code uses the xgboost library to train a more accurate model. It runs a parallel hyperparameter sweep to train multiple models in parallel, using Hyperopt and SparkTrials. As before, the code tracks the performance of each parameter configuration with MLflow.
+
+from hyperopt import fmin, tpe, hp, SparkTrials, Trials, STATUS_OK
+from hyperopt.pyll import scope
+from math import exp
+import mlflow.xgboost
+import numpy as np
+import xgboost as xgb
+ 
+search_space = {
+  'max_depth': scope.int(hp.quniform('max_depth', 4, 100, 1)),
+  'learning_rate': hp.loguniform('learning_rate', -3, 0),
+  'reg_alpha': hp.loguniform('reg_alpha', -5, -1),
+  'reg_lambda': hp.loguniform('reg_lambda', -6, -1),
+  'min_child_weight': hp.loguniform('min_child_weight', -1, 3),
+  'objective': 'binary:logistic',
+  'seed': 123, # Set a seed for deterministic training
+}
+ 
+def train_model(params):
+  # With MLflow autologging, hyperparameters and the trained model are automatically logged to MLflow.
+  mlflow.xgboost.autolog()
+  with mlflow.start_run(nested=True):
+    train = xgb.DMatrix(data=X_train, label=y_train)
+    test = xgb.DMatrix(data=X_test, label=y_test)
+    # Pass in the test set so xgb can track an evaluation metric. XGBoost terminates training when the evaluation metric
+    # is no longer improving.
+    booster = xgb.train(params=params, dtrain=train, num_boost_round=1000,\
+                        evals=[(test, "test")], early_stopping_rounds=50)
+    predictions_test = booster.predict(test)
+    auc_score = roc_auc_score(y_test, predictions_test)
+    mlflow.log_metric('auc', auc_score)
+ 
+    signature = infer_signature(X_train, booster.predict(train))
+    mlflow.xgboost.log_model(booster, "model", signature=signature)
+    
+    # Set the loss to -1*auc_score so fmin maximizes the auc_score
+    return {'status': STATUS_OK, 'loss': -1*auc_score, 'booster': booster.attributes()}
+ 
+# Greater parallelism will lead to speedups, but a less optimal hyperparameter sweep. 
+# A reasonable value for parallelism is the square root of max_evals.
+spark_trials = SparkTrials(parallelism=10)
+ 
+# Run fmin within an MLflow run context so that each hyperparameter configuration is logged as a child run of a parent
+# run called "xgboost_models" .
+with mlflow.start_run(run_name='xgboost_models'):
+  best_params = fmin(
+    fn=train_model, 
+    space=search_space, 
+    algo=tpe.suggest, 
+    max_evals=96,
+    trials=spark_trials, 
+    rstate=np.random.RandomState(123)
+  )
+
+# COMMAND ----------
+
+# DBTITLE 1,Use MLflow to view the results
+# MLflow tracks the parameters and performance metrics of each run. Click the External Link icon  at the top of the Experiment Runs sidebar to navigate to the MLflow Runs Table.
+
+#You used MLflow to log the model produced by each hyperparameter configuration. The following code finds the best performing run and saves the model to Model Registry.
+
+best_run = mlflow.search_runs(order_by=['metrics.auc DESC']).iloc[0]
+print(f'AUC of Best Run: {best_run["metrics.auc"]}')
+
+# COMMAND ----------
+
+# DBTITLE 1,Update the production wine_quality model in MLflow Model Registry
+# Earlier, you saved the baseline model to Model Registry with the name wine_quality. Now that you have a created a more accurate model, update wine_quality.
+
+new_model_version = mlflow.register_model(f"runs:/{best_run.run_id}/model", model_name)
+ 
+# Registering the model takes a few seconds, so add a small delay
+time.sleep(15)
+
+# COMMAND ----------
+
+# Archive the old model version
+client.transition_model_version_stage(
+  name=model_name,
+  version=model_version.version,
+  stage="Archived"
+)
+ 
+# Promote the new model version to Production
+client.transition_model_version_stage(
+  name=model_name,
+  version=new_model_version.version,
+  stage="Production"
+)
+
+# COMMAND ----------
+
+# Clients that call load_model now receive the new model.
+
+# This code is the same as the last block of "Building a Baseline Model". No change is required for clients to get the new model!
+model = mlflow.pyfunc.load_model(f"models:/{model_name}/production")
+print(f'AUC: {roc_auc_score(y_test, model.predict(X_test))}')
+
+# COMMAND ----------
+
+# DBTITLE 1,Batch inference
+# There are many scenarios where you might want to evaluate a model on a corpus of new data. For example, you may have a fresh batch of data, or may need to compare the performance of two models on the same corpus of data.
+
+# To simulate a new corpus of data, save the existing X_train data to a Delta table. 
+# In the real world, this would be a new batch of data.
+spark_df = spark.createDataFrame(X_train)
+# Replace <username> with your username before running this cell.
+table_path = "dbfs:/siyali/delta/wine_data"
+# Delete the contents of this path in case this cell has already been run
+dbutils.fs.rm(table_path, True)
+spark_df.write.format("delta").save(table_path)
+
+
+# COMMAND ----------
+
+# DBTITLE 1,Load the model into a Spark UDF, so it can be applied to the Delta table.
+import mlflow.pyfunc
+ 
+apply_model_udf = mlflow.pyfunc.spark_udf(spark, f"models:/{model_name}/production")
+# Read the "new data" from Delta
+new_data = spark.read.format("delta").load(table_path)
+
+display(new_data)
+
+# COMMAND ----------
+
+from pyspark.sql.functions import struct
+ 
+# Apply the model to the new data
+udf_inputs = struct(*(X_train.columns.tolist()))
+ 
+new_data = new_data.withColumn(
+  "prediction",
+  apply_model_udf(udf_inputs)
+)
+
+display(new_data)
+
+# COMMAND ----------
+
+# DBTITLE 1,Model serving
+import os
+os.environ["DATABRICKS_TOKEN"] = "dapi595aebb786ba02a7ab58fde6b605d508"
+
+# COMMAND ----------
+
+# Replace with code snippet from the model serving page
+import os
+import requests
+import pandas as pd
+ 
+def score_model(dataset: pd.DataFrame):
+  url = 'https://<DATABRICKS_URL>/model/wine_quality/Production/invocations'
+  
+  headers = {'Authorization': f'Bearer {os.environ.get("DATABRICKS_TOKEN")}'}
+  data_json = dataset.to_dict(orient='split')
+  response = requests.request(method='POST', headers=headers, url=url, json=data_json)
+  if response.status_code != 200:
+    raise Exception(f'Request failed with status {response.status_code}, {response.text}')
+  return response.json()
+
+# COMMAND ----------
+
+# Model serving is designed for low-latency predictions on smaller batches of data
+num_predictions = 5
+# served_predictions = score_model(X_test[:num_predictions])
+model_evaluations = model.predict(X_test[:num_predictions])
+# Compare the results from the deployed model and the trained model
+pd.DataFrame({
+  "Model Prediction": model_evaluations,
+#  "Served Model Prediction": served_predictions,
+})
+
+# COMMAND ----------
 
